61ca6104b9627f3d5dff53f893b764198a9b4ec3
Ankit Mithbavkar <72635696+AnkhM@users.noreply.github.com>
2025-06-21 13:15:45 -0400

    Added curl command guide and data validation for the output

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 
+"""
+curl commands:
+
+Post with JSON Payload:
+
+curl -X POST -H "Content-Type: application/json" -d "{\"text\":\"apple\"}" "http://127.0.0.1:8000/items"
+
+Post:
+
+curl -X POST -h "Content-Type: application/json" "http://127.0.0.1:8000/items?item=apple"
+
+Get:
+
+curl -X GET http://127.0.0.1:8000/item/0
+
+"""
 app = FastAPI()
 
 class Item(BaseModel):
@@ -13,17 +29,16 @@ items = []
 def root():
     return {"Hello" : "World"}
 
-
 @app.post("/items")
 def create_item(item: Item):
     items.append(item)
     return items
 
-@app.get("/items")
+@app.get("/items", response_model=list[Item])
 def list_items(limit: int=10):
     return items[0:limit]
 
-@app.get("/items/{item_id}")
+@app.get("/items/{item_id}", response_model=Item)
 def get_item(item_id: int) -> Item:
     if item_id < len(items):
         return items[item_id]
